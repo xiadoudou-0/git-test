7b16c4961d671bd3c316ab7b003dd7986ce220f4
liqun <2682587910@qq.com>
2021-05-10 11:43:34 +0800

    master commit4

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -10,6 +10,6 @@ public class GitTest {
         System.out.println("hello git!02");
         System.out.println("hello git!03");
         System.out.println("hello git!04");
-        System.out.println("hot-fix test!");
+        System.out.println("master test!");
     }
 }
